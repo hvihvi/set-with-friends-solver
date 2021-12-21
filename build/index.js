a859c7f8949ea6ce10ec8ff4fa69bdbc36c9d66d
hvi <hvi@lesfurets.com>
2021-12-21 20:51:59 +0100

    Fix ignored file

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var scrapper_1 = require("./src/scrapper");
-var solve_1 = require("./src/solve");
-console.log((0, solve_1.solve)((0, scrapper_1.scrapCards)()));
