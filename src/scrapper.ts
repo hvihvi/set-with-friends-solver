9b062e83525aa31c03bb6ad7ee5ef83d74bab54c
rqu <roman.quelen@lesfurets.com>
2021-12-22 16:18:10 +0100

    be strict with typecript

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,50 +1,43 @@
-import { Card, Color, Motif, Shape } from "./types";
+import {Card, Color, Motif, Shape} from "./types";
 
 const mapCardElementToNumber = (card: Element) =>
-  // @ts-ignore
-  card.firstChild.childElementCount - 1 ?? 0;
+    (card.firstChild as HTMLDivElement).childElementCount - 1;
 
 const mapCardElementToShape = (card: Element) => {
-  // @ts-ignore
-  const shape = card.firstChild.firstChild.firstChild.getAttribute("href");
-  if (shape === "#squiggle") return Shape.Squiggle;
-  if (shape === "#oval") return Shape.Circle;
-  if (shape === "#diamond") return Shape.Square;
-  throw new Error();
+  const svgElement = card.firstChild!.firstChild!.firstChild as SVGElement;
+  const shape = svgElement.getAttribute("href");
+    if (shape === "#squiggle") return Shape.Squiggle;
+    if (shape === "#oval") return Shape.Circle;
+    if (shape === "#diamond") return Shape.Square;
+    throw new Error();
 };
 
 const mapCardElementToColor = (card: Element) => {
-  // @ts-ignore
-  const color = card.firstChild.firstChild.children[1].getAttribute("stroke");
-  if (color === "#800080") return Color.Violet;
-  if (color === "#008002") return Color.Green;
-  if (color === "#ff0101") return Color.Red;
-  throw new Error();
+  const svgElement = (card.firstChild!.firstChild as SVGElement).children[1] as SVGElement;
+  const color = svgElement.getAttribute("stroke");
+    if (color === "#800080") return Color.Violet;
+    if (color === "#008002") return Color.Green;
+    if (color === "#ff0101") return Color.Red;
+    throw new Error();
 };
 
 const mapCardElementToMotif = (card: Element) => {
-  if (
-    // @ts-ignore
-    card.firstChild.firstChild.children[0].getAttribute("fill") ===
-    "transparent"
-  )
-    return Motif.Transparent;
-  if (
-    // @ts-ignore
-    card.firstChild.firstChild.children[0]
-      .getAttribute("mask")
-      .includes("mask-stripe")
-  )
-    return Motif.Stripe;
-  return Motif.Full;
+    const svgElement = card.firstChild!.firstChild!.firstChild as SVGElement;
+    if (svgElement.getAttribute("fill") === "transparent") {
+        return Motif.Transparent;
+    }
+    if (svgElement.getAttribute("mask")!.includes("mask-stripe")) {
+        return Motif.Stripe;
+    }
+    return Motif.Full;
 };
 
 export const scrapCards = (): Card[] =>
-  Array.from(
-    document.querySelectorAll('div[style*="visibility: visible"]')
-  ).map((card) => ({
-    shape: mapCardElementToShape(card),
-    number: mapCardElementToNumber(card),
-    motif: mapCardElementToMotif(card),
-    color: mapCardElementToColor(card),
-  }));
+    Array.from(
+        document.querySelectorAll('div[style*="visibility: visible"]')
+    ).map((card) => ({
+        shape: mapCardElementToShape(card),
+        number: mapCardElementToNumber(card),
+        motif: mapCardElementToMotif(card),
+        color: mapCardElementToColor(card),
+    }));
