0b6e1091821b5ea60dce6baa8a469ecfa56c43aa
hvi <hvi@lesfurets.com>
2021-12-22 11:43:31 +0100

    Refactor find missing dimension

--- src/solve.ts
+++ src/solve.ts
@@ -23,14 +23,12 @@ export const solve = (cards: Card[]): Solution => {
   ];
 };
 
-export const findMissing = (card1: Card, card2: Card) => {
-  return {
-    color: findMissingDimension(card1.color, card2.color),
-    motif: findMissingDimension(card1.motif, card2.motif),
-    number: findMissingDimension(card1.number, card2.number),
-    shape: findMissingDimension(card1.shape, card2.shape),
-  };
-};
+export const findMissing = (card1: Card, card2: Card) => ({
+  color: findMissingDimension(card1.color, card2.color),
+  motif: findMissingDimension(card1.motif, card2.motif),
+  number: findMissingDimension(card1.number, card2.number),
+  shape: findMissingDimension(card1.shape, card2.shape)
+});
 
 export const findMissingDimension = (d1: 0 | 1 | 2, d2: 0 | 1 | 2) =>
-  (-d1 - d2 + 12) % 3;
+  (3 - ((d1 + d2) % 3)) % 3;
