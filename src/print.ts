d5253ca654ac2101f41f84212790fd4f65116315
hvi <hvi@lesfurets.com>
2021-12-23 09:55:54 +0100

    add printer

--- src/print.ts
+++ src/print.ts
@@ -1,16 +1,23 @@
 import { Card, Color, Motif, Shape, Solution } from "./types";
 
 // TODO utiliser des smiley?
-export const format = (cards: Solution) =>
-  "" +
-  toReadableCard(cards[0]) +
-  toReadableCard(cards[1]) +
-  toReadableCard(cards[2]);
+export const print = (cards: Solution) => {
+  console.group("solution");
+  cards.map((card) =>
+    console.log(
+      "%c" + toReadableCard(card),
+      `font-weight: bold; font-size: 50px;${toReadableColor(
+        card.color
+      )}; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)`
+    )
+  );
+  console.groupEnd();
+};
 
 const toReadableCard = (card: Card) => {
   return `${toReadableShape(card.shape).repeat(
     card.number + 1
-  )} ${toReadableColor(card.color)} ${toReadableMotif(card.motif)}
+  )} ${toReadableMotif(card.motif)}
   `;
 };
 
@@ -28,11 +35,11 @@ const toReadableShape = (input: Shape) => {
 const toReadableColor = (input: Color) => {
   switch (input) {
     case Color.Green:
-      return "vert";
+      return "color:green";
     case Color.Red:
-      return "rouge";
+      return "color:red";
     case Color.Violet:
-      return "violet";
+      return "color:violet";
   }
 };
 
